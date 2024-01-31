15da34e3e1f01a74bfec9273bd38ceea2419ff89
Prasad More <prasadmoremain@gmail.com>
2024-01-31 16:47:49 +0530

    Added Form Validation

--- home.js
+++ home.js
@@ -1,11 +1,36 @@
 let startQuiz = document.getElementById("start-quiz")
-
 startQuiz.addEventListener("click",start,false)
 
+let userName = document.getElementById("name")
+let userEmail = document.getElementById("email")
+// let quizSelect = document.getElementById("quiz-select")
+let nameError = document.getElementById("nameError")
+let emailError = document.getElementById("emailError")
+let quizError = document.getElementById("quizError")
 
 function start(){
 
     let quizSelect = document.getElementById("quiz-select").value
+   
+    if(userName.value==""){
+        nameError.innerText="Please Enter Your Name!"
+    } else {
+        nameError.style.display="none"
+    }
+    
+    if(userEmail.value==""){
+        emailError.innerHTML="Please Enter Your Email Id!"
+    } else {
+        emailError.style.display="none"
+    }
+
+    if(quizSelect=="" || quizSelect=="Select the Quiz"){
+        quizError.innerHTML="Please Select the Quiz"
+    } else {
+        quizError.style.display="none"
+    }
+
+   
     if(quizSelect == "Technology"){
         location.assign("technologyQuiz.html")
     } 
