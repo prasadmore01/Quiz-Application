b30765875783f97f51c3d6939727df0355e3583e
Prasad More <prasadmoremain@gmail.com>
2024-01-31 16:12:11 +0530

    Working on Home page Javascript file

---
+++ home.js
@@ -0,0 +1,12 @@
+let startQuiz = document.getElementById("start-quiz")
+
+startQuiz.addEventListener("click",start,false)
+
+
+let start = ()=>{
+
+    let quizSelect = document.getElementById("quiz-select").value
+
+
+
+}
